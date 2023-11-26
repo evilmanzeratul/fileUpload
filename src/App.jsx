8c74202579db415d290f54553bdf18ed9a3ad0ca
Grzes <evilmanzeratul@gmail.com>
2023-11-26 19:10:07 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import { storage } from './firebase'
-import { ref, uploadBytes } from 'firebase/storage'
+import { ref, uploadBytes, listAll, getDownloadURL } from 'firebase/storage'
 import { v4 } from 'uuid'
 
 function App() {
 
   const [imageUpload, setImageUpload] = useState(null)
+  const [imageList, setImageList] = useState([])
+
+  const imagesListRef = ref(storage, "images/")
 
   async function uploadImage() {
     if (!imageUpload) return;
@@ -20,11 +23,42 @@ function App() {
 
   }
 
+  async function getImage() {
+    try {
+      const response = await listAll(imagesListRef)
+      response.items.forEach(async (item) => {
+        const url = await getDownloadURL(item)
+        setImageList((prev) => [...prev, url])
+      });
+      console.log(imageList);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  useEffect(() => {
+    getImage()
+  }, [])
+  // useEffect(() => {
+  //   listAll(imagesListRef).then((response) => {
+  //     response.items.forEach((item) => {
+  //       getDownloadURL(item).then((url) => {
+  //         setImageList((prev) => [...prev, url]);
+  //       });
+  //     });
+  //   });
+  // }, []);
+
+  console.log(imageList)
+
   return (
     <div className="App">
       <input type="file" onChange={(e) => { setImageUpload(e.target.files[0]) }} />
 
       <button onClick={uploadImage}>Upload Image</button>
+
+      {imageList.map((url) => {
+        return <img src={url} />
+      })}
     </div>
   );
 }
